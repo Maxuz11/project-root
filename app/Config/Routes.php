cbaa33c65cc96d1ac7245cbc880c93a708b6e08d
Mjeldresu <m.jeldres@duocuc.cl>
2023-12-25 12:32:40 -0300

    intento2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-<<<<<<< HEAD
-=======
-//$routes->get('/', 'Home');
->>>>>>> df7b122b1151d526a7ae1e2a74225475ff4e63fb
+

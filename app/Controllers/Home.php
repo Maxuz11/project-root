92fb0405a5d171e952311125db8f9072c6a9b77e
Mjeldresu <m.jeldres@duocuc.cl>
2023-12-25 13:17:04 -0300

    para q corrar local

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        //return view('portfolio');
-        return redirect()->to(base_url('portfolio.html'));
+        return view('portfolio');
     }
 }
